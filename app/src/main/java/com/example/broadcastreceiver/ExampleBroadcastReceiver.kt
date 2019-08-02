5a4a44d5be681bfadea237ec74757932b30edce6
Yarosh Andrii <>
2019-08-02 10:42:08 +0300

    first commit

---
+++ app/src/main/java/com/example/broadcastreceiver/ExampleBroadcastReceiver.kt
@@ -0,0 +1,16 @@
+package com.example.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+/**
+ * Copyright (c) Created by Andrii Yarosh.
+ * https://dstarlab.com/
+ */
+class ExampleBroadcastReceiver: BroadcastReceiver() {
+
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+    }
+}
